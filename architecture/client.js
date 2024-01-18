7b90c9f25609b4070eed1156c8a16892e69d3104
KangJordan <90824882+JordanRuili@users.noreply.github.com>
2024-01-18 11:59:57 +0100

    CQRS 3 premiers exos

--- architecture/client.js
+++ architecture/client.js
@@ -1,10 +1,19 @@
-const { updatePatient } = require('./patientDAO');
 const patientService = require('./patientService');
+const patientCommand = require('./patientCommand');
+const patientQuery = require('./patientQuery');
 
+// CRUD
 patientService.addPatient('toto', 'titi');
 console.log(patientService.getPatientList());
+
 patientService.savePatient(1,'toto','tata');;
 console.log(patientService.getPatientList());
 
-patientService.getPatient(1);
-console.log(patientService.getPatient(1))
+console.log(patientService.getPatient(1));
+
+//CQRS
+patientCommand.addPatient('moi','moi');
+
+patientQuery.getPatientList();
+
+patientQuery.getPatient(2);
